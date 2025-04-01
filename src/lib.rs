1c1243ae1666644d486697d5c8752b6f575b484c
Tom <tom.b.montgomery@gmail.com>
2025-04-01 19:33:04 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+// Constuctor for Config struct
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Too few arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.file_path)?;
+    println!("With text: \n{content}");
+    Ok(())
+}
+
