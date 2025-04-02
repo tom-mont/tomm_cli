2cbda2efcd335a5840eeca35c49b9c0cc78b617c
Tom <tom.b.montgomery@gmail.com>
2025-04-02 19:57:46 +0200

    working

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ use tomm_cli::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    // First item in the vector is the name of the
-    // bin, hence we start at [1]:
+
     if let Err(e) = tomm_cli::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
