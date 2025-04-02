044fd1c8ba8ed00a76ef3a02046e7443e0062b78
Tom <tom.b.montgomery@gmail.com>
2025-04-02 07:52:58 +0200

    building tests

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,11 @@ use tomm_cli::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    // First item in the vector is the name of the 
+    // First item in the vector is the name of the
     // bin, hence we start at [1]:
-    println!("Searching for: {}", config.query);
-    println!("In file: {}", config.file_path);
     if let Err(e) = tomm_cli::run(config) {
-        println!("Application error: {e}");
         process::exit(1);
     }
 }
-
